0b5c3b9b26a557829a7c24e94a6e6352375f3f4d
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-13 04:37:08 +0900

    feat: ディレクトリ階層を上がるボタン

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,16 +1,26 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Layout from '../components/Layout'
 import ListFiles from '../components/ListFiles'
+import path from 'path'
 
 const FilesListPage = () => {
-  const { query } = useRouter()
+  const router = useRouter()
+  const { query } = router
   const oldDir = Array.isArray(query.olddir) ? query.olddir[0] : query.olddir
   const newDir = Array.isArray(query.newdir) ? query.newdir[0] : query.newdir
 
   return (
     <Layout title="Files List">
       <h1>Files List</h1>
+
+      <button
+        onClick={() => {
+          router.push(`?olddir=${path.dirname(oldDir)}&newdir=${path.dirname(newDir)}`)
+        }}
+      >
+        ⬆
+      </button>
+
       <ListFiles oldDir={oldDir} newDir={newDir} />
     </Layout>
   )
