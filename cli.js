12f9de80de64e3106b7998f5d3fa47d691ea2ad1
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-20 21:59:42 +0900

    chore: open廃止、binプロパティ追加

--- cli.js
+++ cli.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process')
-const open = require('open')
 
 spawn('next', ['dev'], { cwd: __dirname, stdio: 'inherit' })
-open('http://localhost:3000/')
